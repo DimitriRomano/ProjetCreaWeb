1b6b85808c4be51814666a7c3acc7e3e8b53409c
R_o_h_t <r.marieh@hotmail.com>
2022-03-08 10:55:32 +0100

    added Roboto Font

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -6,6 +6,8 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+  defaultColor = '#90caf9';
+  defaultRadius = '8px';
   @Input()
   click() {}
 
@@ -22,25 +24,27 @@ export class ButtonComponent implements OnInit {
   respectCase: boolean = false;
 
   @Input()
-  color: string = '#a1ce97';
+  color: string = this.defaultColor;
 
   @Input()
-  borderRadius: string = '8px';
+  borderRadius: string = this.defaultRadius;
 
   @Input()
   fullWidth: boolean = false;
+
+  hover = false;
 
   ngOnInit(): void {}
 
   get style() {
     const usedColor = CSS.supports('color', this.color)
       ? this.color
-      : '#a1ce97';
+      : this.defaultColor;
     const usedBorderRadius = CSS.supports('borderRadius', this.borderRadius)
       ? this.borderRadius
-      : '8px';
+      : this.defaultRadius;
     return {
-      color: this.variant === 'contained' ? '#818181' : usedColor,
+      color: this.variant === 'contained' ? '#F7F7F7' : usedColor,
       border: this.variant === 'outlined' ? '1px solid' : 'none',
       borderColor: usedColor,
       backgroundColor: this.variant === 'contained' ? usedColor : 'none',
@@ -49,6 +53,7 @@ export class ButtonComponent implements OnInit {
       cursor: 'pointer',
       padding: '6px 8px',
       width: 'fit-content',
+      filter: this.hover ? 'brightness(110%)' : 'none',
     };
   }
 }
