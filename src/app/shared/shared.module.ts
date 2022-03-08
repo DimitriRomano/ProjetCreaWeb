5e1ccaaaef240a568eb721ab779fb6b76c31f1a6
MolinaRomain <molinaromain04@gmail.com>
2022-03-08 12:17:04 +0100

    Creation of the navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './button/button.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ButtonComponent} from './button/button.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [ButtonComponent, NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [ButtonComponent, NavbarComponent],
 })
-export class SharedModule {}
+export class SharedModule {
+}
