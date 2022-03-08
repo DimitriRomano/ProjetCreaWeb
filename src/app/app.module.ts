6de2a0cc404d49613e121c874f29e41cbce67a95
R_o_h_t <r.marieh@hotmail.com>
2022-03-08 10:42:47 +0100

    fix error in dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { SharedModule } from './shared/shared.module';
-=======
-import 'dotenv/config';
->>>>>>> 373a2f9a3f042337dc95b083eb2a86e7cc9bf921
 
 @NgModule({
   declarations: [AppComponent],
