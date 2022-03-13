599fc84765a2cbcb69315620887d040490cf24f8
BONNIN <francois.bonnin@ynov.com>
2022-03-13 23:45:19 +0100

    ng g c card
    create folder in shared component and generate component card-simple

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardSimpleComponent } from './card/card-simple/card-simple.component';
 
 @NgModule({
-  declarations: [ButtonComponent, NavbarComponent],
+  declarations: [ButtonComponent, NavbarComponent, CardSimpleComponent],
   imports: [CommonModule],
   exports: [ButtonComponent, NavbarComponent],
 })
