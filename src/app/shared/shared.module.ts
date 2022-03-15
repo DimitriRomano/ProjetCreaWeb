3e6a973203a1d0d99adab7b96712c89b6df61ef6
R_o_h_t <r.marieh@hotmail.com>
2022-03-15 10:12:37 +0100

    merged with footer&header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,26 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ButtonComponent} from './button/button.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {RouterModule} from "@angular/router";
-import {FooterComponent} from './footer/footer.component';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button/button.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 import { CardTextComponent } from './card/card-text/card-text.component';
 import { CardImageComponent } from './card/card-image/card-image.component';
 
 @NgModule({
-  declarations: [ButtonComponent, NavbarComponent,FooterComponent, CardTextComponent, CardImageComponent],
+  declarations: [
+    ButtonComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardTextComponent,
+    CardImageComponent,
+  ],
   imports: [CommonModule],
-  exports: [ButtonComponent, NavbarComponent, CardTextComponent, CardImageComponent],
+  exports: [
+    ButtonComponent,
+    NavbarComponent,
+    CardTextComponent,
+    CardImageComponent,
+  ],
 })
-export class SharedModule {
-}
+export class SharedModule {}
