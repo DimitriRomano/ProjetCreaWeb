2b995df50f7cf9f67d2d35d8ac124308cc0bbf2f
DimitriRomano <dimitri.romano09@gmail.com>
2022-03-20 23:29:31 +0100

    lib mapgl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { MapModule } from './map/map.module';
 
 import { ProfileModule } from './profile/profile.module';
 
