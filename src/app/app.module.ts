ae7f5de28a27cdbfc86a8a50537244eab35b3e2c
DimitriRomano <dimitri.romano09@gmail.com>
2022-03-20 23:22:59 +0100

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,10 @@ import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { ProfileModule } from './profile/profile.module';
 
 
-<<<<<<< HEAD
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, FontAwesomeModule,ProfileModule],
-=======
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MapModule } from './map/map.module';
-import { HttpClientModule } from '@angular/common/http';
-
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MapModule, HttpClientModule],
->>>>>>> map
+  imports: [BrowserModule, AppRoutingModule, SharedModule, FontAwesomeModule,ProfileModule, MapModule],
   providers: [],
   bootstrap: [AppComponent],
 })
