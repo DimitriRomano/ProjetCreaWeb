9b02306a8b3bb2efab59b4fd68e45ec66e68429c
DimitriRomano <dimitri.romano09@gmail.com>
2022-03-21 14:57:37 +0100

    pb merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { MapModule } from './map/map.module';
-
+import { HttpClientModule } from '@angular/common/http';
 import { ProfileModule } from './profile/profile.module';
-
-
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, FontAwesomeModule,ProfileModule, MapModule],
+  imports: [BrowserModule, AppRoutingModule, MapModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
