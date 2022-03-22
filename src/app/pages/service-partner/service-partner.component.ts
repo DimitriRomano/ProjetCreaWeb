37e6321d62b8705b7cff5639832a0a9a2a5c22b9
BONNIN <francois.bonnin@ynov.com>
2022-03-22 02:07:13 +0100

    Page Service - Partner Finished :à relier au router Need Swiper

--- src/app/pages/service-partner/service-partner.component.ts
+++ src/app/pages/service-partner/service-partner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-service-partner',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-partner.component.scss']
 })
 export class ServicePartnerComponent implements OnInit {
+
+  title = "Liste des partenaires.";
+  paragraph1 = "Nos partenaires vous assure une consommation durable et responsable.";
+  paragraph2 = "Découvrez les avantages Cycle qu’ils proposent et leurs derniers trophées.";
 
   constructor() { }
 
